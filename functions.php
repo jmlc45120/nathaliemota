939e982f31bf4e21427cc002377c04c2095da750
Jean-Michel LE COUVIOUR <jeanmilc45@gmail.com>
2024-10-04 11:02:37 +0200

    Mise en place des fichiers de base du thème

--- functions.php
+++ functions.php
@@ -1,5 +1,14 @@
 <?php
-function theme_nathaliemota_enqueue_styles() {
+function theme_nathaliemota_enqueue_styles()
+{
     wp_enqueue_style('main-style', get_stylesheet_uri());
 }
 add_action('wp_enqueue_scripts', 'theme_nathaliemota_enqueue_styles');
+
+function register_my_menus()
+{
+    register_nav_menus(array(
+        'main-menu' => __('Menu Principal'),
+    ));
+}
+add_action('init', 'register_my_menus');

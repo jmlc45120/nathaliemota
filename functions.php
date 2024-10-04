2a77f316d369cad9c662bf35d71fb4862ce85f52
Jean-Michel LE COUVIOUR <jeanmilc45@gmail.com>
2024-10-04 11:04:03 +0200

    ajout des polices Google Fonts

--- functions.php
+++ functions.php
@@ -1,9 +1,13 @@
 <?php
-function theme_nathaliemota_enqueue_styles()
-{
+function theme_nathaliemota_enqueue_styles() {
+    // Charger les polices Google Fonts
+    wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&family=Poppins:wght@300&display=swap', false);
+    // Charger le fichier CSS principal
     wp_enqueue_style('main-style', get_stylesheet_uri());
 }
 add_action('wp_enqueue_scripts', 'theme_nathaliemota_enqueue_styles');
+
+//-----------------------------------------------------------------------
 
 function register_my_menus()
 {

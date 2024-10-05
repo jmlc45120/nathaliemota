62e5397d7bddbe763e62f3988ba4d19571ce3602
Jean-Michel LE COUVIOUR <jeanmilc45@gmail.com>
2024-10-05 10:26:53 +0200

    mise en place structure style.scss et _variables (couleurs du site)

--- functions.php
+++ functions.php
@@ -2,8 +2,8 @@
 function theme_nathaliemota_enqueue_styles() {
     // Chargement des polices Google Fonts
     wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&family=Poppins:wght@300&display=swap', false);
-    // Chargement du fichier CSS principal
-    wp_enqueue_style('main-style', get_stylesheet_uri());
+    // Chargement du fichier CSS généré à partir du scss 
+    wp_enqueue_style('main-style', get_template_directory_uri() . '/assets/css/style.css');
 }
 add_action('wp_enqueue_scripts', 'theme_nathaliemota_enqueue_styles');
 
--- template-parts/header.php
+++ template-parts/header.php
@@ -6,13 +6,13 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-<header class="site-header">
+<header class="header__container">
     <div class="logo">
         <a href="<?php echo home_url(); ?>">
             <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="Logo du site">
         </a>
     </div>
-    <nav class ="site-nav">
+    <nav class ="header__nav">
         <?php
         wp_nav_menu( array(
             'theme_location' => 'main-menu',
